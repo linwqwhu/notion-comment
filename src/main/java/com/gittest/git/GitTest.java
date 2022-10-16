b95ae93e901b43b384b53c427c6be1ff8490f445
linwqwhu <2876865842@qq.com>
2022-10-16 10:37:57 +0800

    third commit

--- src/main/java/com/gittest/git/GitTest.java
+++ src/main/java/com/gittest/git/GitTest.java
@@ -3,5 +3,7 @@ package com.gittest.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello github!");
+        System.out.println("hello git3!");
     }
 }
